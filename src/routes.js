b2d3149878174e451466cdee732ee0d676cf85a5
João Vitor Sousa <joaovfsousa@gmail.com>
2021-03-04 18:44:41 -0300

    Add verifyJWT middleware in all routes

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,17 @@ var express = require('express')
 var router = express.Router()
 var UserController = require('./controllers/UserController')
 var AuthController = require('./controllers/AuthController')
+var verifyJWT = require('./helpers/verifyJWT');
 
 // login routes
 router.post('/login', AuthController.login)
-router.get('/logout', AuthController.logout)
+router.get('/logout', verifyJWT, AuthController.logout)
 
 // users routes
-router.get('/users', UserController.index)
-router.get('/users/:id', UserController.show)
-router.post('/users', UserController.store)
-router.put('/users/:id', UserController.update)
-router.delete('/users/:id', UserController.delete)
+router.get('/users', verifyJWT, UserController.index)
+router.get('/users/:id', verifyJWT, UserController.show)
+router.post('/users', verifyJWT, UserController.store)
+router.put('/users/:id', verifyJWT, UserController.update)
+router.delete('/users/:id', verifyJWT, UserController.delete)
 
 module.exports = router;
