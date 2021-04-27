db847bad44f9a0a0c1f3fa425ebe8f98e17b4c08
João Vitor Sousa <joaovf.sousa@obers.com.br>
2021-04-27 19:11:44 -0300

    Adiciona semicolon nos arquivos

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const router = require('./routes')
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
 
 require('./database');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(router)
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
-module.exports = app
+module.exports = app;
--- src/routes.js
+++ src/routes.js
@@ -1,51 +1,51 @@
-var express = require('express')
-var router = express.Router()
-var UserController = require('./controllers/UserController')
-var AuthController = require('./controllers/AuthController')
-var EnvironmentController = require('./controllers/EnvironmentController')
-var ProductController =  require('./controllers/ProductController')
-var OrderController = require('./controllers/OrderController')
-var TabController = require('./controllers/TabController')
+var express = require('express');
+var router = express.Router();
+var UserController = require('./controllers/UserController');
+var AuthController = require('./controllers/AuthController');
+var EnvironmentController = require('./controllers/EnvironmentController');
+var ProductController =  require('./controllers/ProductController');
+var OrderController = require('./controllers/OrderController');
+var TabController = require('./controllers/TabController');
 var verifyJWT = require('./helpers/verifyJWT');
 
 // login routes
-router.post('/login', AuthController.login)
-router.get('/logout', verifyJWT, AuthController.logout)
-router.get('/renew', verifyJWT, AuthController.renew)
+router.post('/login', AuthController.login);
+router.get('/logout', verifyJWT, AuthController.logout);
+router.get('/renew', verifyJWT, AuthController.renew);
 
 // users routes
-router.get('/users', verifyJWT, UserController.index)
-router.get('/users/:id', verifyJWT, UserController.show)
-router.post('/users', verifyJWT, UserController.store)
-router.put('/users/:id', verifyJWT, UserController.update)
-router.delete('/users/:id', verifyJWT, UserController.delete)
+router.get('/users', verifyJWT, UserController.index);
+router.get('/users/:id', verifyJWT, UserController.show);
+router.post('/users', verifyJWT, UserController.store);
+router.put('/users/:id', verifyJWT, UserController.update);
+router.delete('/users/:id', verifyJWT, UserController.delete);
 
 // environments routes
-router.post('/environments', verifyJWT, EnvironmentController.store)
-router.get('/environments', verifyJWT, EnvironmentController.index)
-router.get('/environments/:id', verifyJWT, EnvironmentController.show)
-router.put('/environments/:id', verifyJWT, EnvironmentController.update)
-router.delete('/environments/:id', verifyJWT, EnvironmentController.delete)
+router.post('/environments', verifyJWT, EnvironmentController.store);
+router.get('/environments', verifyJWT, EnvironmentController.index);
+router.get('/environments/:id', verifyJWT, EnvironmentController.show);
+router.put('/environments/:id', verifyJWT, EnvironmentController.update);
+router.delete('/environments/:id', verifyJWT, EnvironmentController.delete);
 
 // products routes
-router.post('/products', verifyJWT, ProductController.store)
-router.get('/products', verifyJWT, ProductController.index)
-router.get('/products/:id', verifyJWT, ProductController.show)
-router.put('/products/:id', verifyJWT, ProductController.update)
-router.delete('/products/:id', verifyJWT, ProductController.delete)
+router.post('/products', verifyJWT, ProductController.store);
+router.get('/products', verifyJWT, ProductController.index);
+router.get('/products/:id', verifyJWT, ProductController.show);
+router.put('/products/:id', verifyJWT, ProductController.update);
+router.delete('/products/:id', verifyJWT, ProductController.delete);
 
 // orders routes
-router.post('/orders', verifyJWT, OrderController.store)
-router.get('/orders', verifyJWT, OrderController.index)
-router.get('/orders/:id', verifyJWT, OrderController.show)
-router.put('/orders/:id', verifyJWT, OrderController.update)
-router.delete('/orders/:id', verifyJWT, OrderController.delete)
+router.post('/orders', verifyJWT, OrderController.store);
+router.get('/orders', verifyJWT, OrderController.index);
+router.get('/orders/:id', verifyJWT, OrderController.show);
+router.put('/orders/:id', verifyJWT, OrderController.update);
+router.delete('/orders/:id', verifyJWT, OrderController.delete);
 
 // tabs routes
-router.post('/tabs', verifyJWT, TabController.store)
-router.get('/tabs', verifyJWT, TabController.index)
-router.get('/tabs/:id', verifyJWT, TabController.show)
-router.put('/tabs/:id', verifyJWT, TabController.update)
-router.delete('/tabs/:id', verifyJWT, TabController.delete)
+router.post('/tabs', verifyJWT, TabController.store);
+router.get('/tabs', verifyJWT, TabController.index);
+router.get('/tabs/:id', verifyJWT, TabController.show);
+router.put('/tabs/:id', verifyJWT, TabController.update);
+router.delete('/tabs/:id', verifyJWT, TabController.delete);
 
 module.exports = router;
